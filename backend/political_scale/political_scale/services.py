af095285475f512e82e6b17a94e929aec50bbff9
James Pawson <U1951078@unimail.hud.ac.uk>
2024-10-26 23:53:31 +0100

    Add a test call to prompt api to create a story

---
+++ backend/political_scale/political_scale/services.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+import requests
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=GEMINI_API_KEY)
+
+def get_data_from_prompt_api():
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content("Write a story about a magic backpack.")
+
+    return response.text
