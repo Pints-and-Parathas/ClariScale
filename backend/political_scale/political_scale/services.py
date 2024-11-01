b850f403ce01ff55c46c140f4d5afb8a6a05ec3d
James Pawson <U1951078@unimail.hud.ac.uk>
2024-11-01 20:23:42 +0000

    Add decoding to get html

--- backend/political_scale/political_scale/services.py
+++ backend/political_scale/political_scale/services.py
@@ -6,20 +6,21 @@ from django.http import JsonResponse
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def get_data_from_prompt_api():
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    
     response = model.generate_content("Write a story about a magic backpack.")
 
     return response.text
 
 
-
 def fetch_article_data(url):
-    pageHTML = requests.get(url)
-    pageHTML.raise_for_status()
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    prompt = f"""Using this html: {pageHTML.text}
+    page_html = requests.get(url)
+    page_html.raise_for_status()
+    html_content = page_html.content.decode('utf-8')
+
+    prompt = f"""Using this html: {html_content}
                 Extract information about the article, disregard anything that is an advert or a link to another article.
                 Provide a response using this JSON scheme: 
                 {{
@@ -33,12 +34,12 @@ def fetch_article_data(url):
     response = model.generate_content(prompt)
     
     try:
-        responseJson = json.loads(response.text)
+        response_json = json.loads(response.text)
     except json.JSONDecodeError as e:
         print("error " + str(e))
-        responseJson = {"error": str(e)}
+        response_json = {"error": str(e)}
     
-    return JsonResponse(responseJson)
+    return JsonResponse(response_json)
 
 def get_data_from_sentiment_api():
     prompt = """Tell me whether the following sentence's sentiment is positive or negative or something in between.
