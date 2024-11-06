49827c509db3105f48169f862e857d5177f68fc2
omham91 <omair.hamza19@gmail.com>
2024-11-06 23:27:18 +0000

    add more details link, changed styling

--- frontend/political_scale/src/popUp/PopUp.styled.js
+++ frontend/political_scale/src/popUp/PopUp.styled.js
@@ -6,6 +6,7 @@ export const PopupContainer = styled.div`
   top: 0px; 
   right: 0px;
   width: 250px;
+  height: 50px;
   padding: 20px;
   background-color: ${(props) => props.theme.background}; /* Use theme for background */
   color: ${(props) => props.theme.color}; /* Use theme for text color */
