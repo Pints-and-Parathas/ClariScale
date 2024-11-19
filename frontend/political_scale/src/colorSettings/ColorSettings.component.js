26bccf1712608cae92bb261eed4f556eeb1a211a
james <jamespawson64@gmail.com>
2024-11-19 21:38:28 +0000

    Add isVisible prop to settings

--- frontend/political_scale/src/colorSettings/ColorSettings.component.js
+++ frontend/political_scale/src/colorSettings/ColorSettings.component.js
@@ -1,13 +1,18 @@
-import React, {useState, useEffect, useRef} from "react";
-import { SettingsWindow, OptionButton, ThemeModal, ThemeModalOverlay } from "./ColorSettings.styled";
-import ThemeToggle from '../themeToggle/ThemeToggle.component';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  SettingsWindow,
+  OptionButton,
+  ThemeModal,
+  ThemeModalOverlay,
+} from "./ColorSettings.styled";
+import ThemeToggle from "../themeToggle/ThemeToggle.component";
 import { ToggleContainer } from "../themeToggle/ThemeToggle.styled";
 import { gradientOptions } from "./GradientColors.styled";
 
-const ColorSettings = ({theme, toggleTheme, onGradientSelect}) => {
+const ColorSettings = ({ theme, toggleTheme, onGradientSelect, isVisible }) => {
   const [isThemeModalVisible, setIsThemeModalVisible] = useState(false); // Control Theme modal visibility
-  const [isGradientModalVisible, setIsGradientModalVisible] = useState(false);// Visibility of gradient modal
-  
+  const [isGradientModalVisible, setIsGradientModalVisible] = useState(false); // Visibility of gradient modal
+
   // Toggle the visibility of the theme modal
   const toggleThemeModal = () => {
     setIsThemeModalVisible(!isThemeModalVisible);
@@ -20,10 +25,16 @@ const ColorSettings = ({theme, toggleTheme, onGradientSelect}) => {
 
   return (
     <>
-      <SettingsWindow >
-        <OptionButton onClick={toggleThemeModal}>Change Appearance</OptionButton>
-        <OptionButton onClick={toggleGradientModal}>Change Gradient</OptionButton>
-      </SettingsWindow>
+      {isVisible && (
+        <SettingsWindow>
+          <OptionButton onClick={toggleThemeModal}>
+            Change Appearance
+          </OptionButton>
+          <OptionButton onClick={toggleGradientModal}>
+            Change Gradient
+          </OptionButton>
+        </SettingsWindow>
+      )}
 
       {isThemeModalVisible && (
         <>
@@ -34,37 +45,37 @@ const ColorSettings = ({theme, toggleTheme, onGradientSelect}) => {
               <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
             </ToggleContainer>
           </ThemeModal>
-      </>
-    )}
+        </>
+      )}
 
-    {isGradientModalVisible && (
-            <>
-              <ThemeModalOverlay onClick={toggleGradientModal} />
-              <ThemeModal isVisible={isGradientModalVisible}>
-                <h3>Select Gradient</h3>
-                <div>
-                  {gradientOptions.map((gradient, index) => (
-                    <OptionButton
-                      key={index}
-                      onClick={() => {
-                        onGradientSelect(gradient); // Set selected gradient
-                        setIsGradientModalVisible(false); // Close modal
-                      }}
-                      style={{
-                        background: gradient,
-                        color: "#fff",
-                        margin: "5px",
-                        padding: "10px",
-                        borderRadius: "5px"
-                      }}
-                    >
-                      Gradient {index + 1}
-                    </OptionButton>
-                  ))}
-                </div>
-              </ThemeModal>
-            </>
-          )}    
+      {isGradientModalVisible && (
+        <>
+          <ThemeModalOverlay onClick={toggleGradientModal} />
+          <ThemeModal isVisible={isGradientModalVisible}>
+            <h3>Select Gradient</h3>
+            <div>
+              {gradientOptions.map((gradient, index) => (
+                <OptionButton
+                  key={index}
+                  onClick={() => {
+                    onGradientSelect(gradient); // Set selected gradient
+                    setIsGradientModalVisible(false); // Close modal
+                  }}
+                  style={{
+                    background: gradient,
+                    color: "#fff",
+                    margin: "5px",
+                    padding: "10px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  Gradient {index + 1}
+                </OptionButton>
+              ))}
+            </div>
+          </ThemeModal>
+        </>
+      )}
     </>
   );
 };
--- frontend/political_scale/src/slidingPanel/SlidingPanel.component.js
+++ frontend/political_scale/src/slidingPanel/SlidingPanel.component.js
@@ -1,73 +1,89 @@
-import React, {useState, useEffect, useRef} from "react";
-import { PanelContainer,PanelContent, CloseButton } from "./SlidingPanel.styled";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  PanelContainer,
+  PanelContent,
+  CloseButton,
+} from "./SlidingPanel.styled";
 import { SettingsIcon } from "../colorSettings/ColorSettings.styled";
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings } from "react-icons/fi";
 import ColorSettings from "../colorSettings/ColorSettings.component";
 
+const SlidingPanel = ({
+  isOpen,
+  children,
+  theme,
+  toggleTheme,
+  onClose,
+  onGradientSelect,
+  selectedGradient,
+}) => {
+  const [arrowState, setArrowState] = useState(isOpen); // Manage state locally
+  const [showSettings, setShowSettings] = useState(false); // Track settings visibility
+  const settingsRef = useRef(null); //Reference for settings window
 
-const SlidingPanel = ({isOpen, children, theme, toggleTheme, onClose, onGradientSelect, selectedGradient}) => {
-    const [arrowState, setArrowState] = useState(isOpen); // Manage state locally
-    const [showSettings, setShowSettings] = useState(false); // Track settings visibility
-    const settingsRef = useRef(null); //Reference for settings window
+  useEffect(() => {
+    setArrowState(isOpen);
+  }, [isOpen]);
 
-    useEffect(() => {
-        setArrowState(isOpen);
-      }, [isOpen]);
+  const togglePanel = () => {
+    setArrowState(!arrowState); // Toggle the state on button click
+  };
 
-    const togglePanel = () => {
-      setArrowState(!arrowState); // Toggle the state on button click
+  const toggleSettings = () => {
+    setShowSettings(!showSettings); // Toggle the settings popup
+  };
+
+  const handleClickOutside = (event) => {
+    // Close the settings if clicking outside it or toggling the settings icon
+    if (
+      settingsRef.current &&
+      !settingsRef.current.contains(event.target) &&
+      !event.target.closest(`.${SettingsIcon.styledComponentId}`)
+    ) {
+      setShowSettings(false);
+    }
+  };
+
+  // Attach event listener to close the settings on outside click
+  useEffect(() => {
+    if (showSettings) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside); // Clean up listener
     };
+  }, [showSettings]);
 
-    const toggleSettings = () => {
-        setShowSettings(!showSettings); // Toggle the settings popup
-    };
-
-    const handleClickOutside = (event) => {
-        // Close the settings if clicking outside it or toggling the settings icon
-        if (
-            settingsRef.current &&
-            !settingsRef.current.contains(event.target) &&
-            !event.target.closest(`.${SettingsIcon.styledComponentId}`)
-        ) {
-            setShowSettings(false);
-        }
-    };
-
-    // Attach event listener to close the settings on outside click
-    useEffect(() => {
-        if (showSettings) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
-    
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside); // Clean up listener
-        };
-    }, [showSettings]);
-
-    return(
-        <>
-            <PanelContainer isOpen={arrowState}>
-                <PanelContent>
-                    <h2 style={{ marginBottom: '40px' }}>Details</h2>
-                    {children}
-                    <p>Insert Details here fr fr.</p>
-                </PanelContent>
-                <SettingsIcon onClick={toggleSettings}>
-                    <FiSettings />
-                </SettingsIcon>
-                {showSettings && (
-                    <div ref={settingsRef}>
-                        <ColorSettings toggleTheme={toggleTheme} theme={theme} onGradientSelect={onGradientSelect}/>
-                    </div>
-                )}
-            </PanelContainer>  
-            <CloseButton isOpen={arrowState} onClick={togglePanel}>
-                <span className="arrow">➔</span> 
-            </CloseButton>
-        </>
-    );
+  return (
+    <>
+      <PanelContainer isOpen={arrowState}>
+        <PanelContent>
+          <h2 style={{ marginBottom: "40px" }}>Details</h2>
+          {children}
+          <p>Insert Details here fr fr.</p>
+        </PanelContent>
+        <SettingsIcon onClick={toggleSettings}>
+          <FiSettings />
+        </SettingsIcon>
+        {showSettings && (
+          <div ref={settingsRef}>
+            <ColorSettings
+              toggleTheme={toggleTheme}
+              theme={theme}
+              onGradientSelect={onGradientSelect}
+              isVisible={showSettings}
+            />
+          </div>
+        )}
+      </PanelContainer>
+      <CloseButton isOpen={arrowState} onClick={togglePanel}>
+        <span className="arrow">➔</span>
+      </CloseButton>
+    </>
+  );
 };
 
 export default SlidingPanel;
