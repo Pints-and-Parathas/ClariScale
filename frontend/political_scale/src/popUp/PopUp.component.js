ba2cb7d1e6b64d4b4bad3c5a9460236de7171260
James Pawson <U1951078@unimail.hud.ac.uk>
2024-11-23 16:31:23 +0000

    Removed unused imports

--- frontend/political_scale/src/popUp/PopUp.component.js
+++ frontend/political_scale/src/popUp/PopUp.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { PopupContainer, DismissButton } from './PopUp.styled';
 import SlidingScale from '../slidingScale/SlidingScale.component';
 import { LearnMoreLink } from '../moreDetailsLink/MoreDetailsLink.styled';
