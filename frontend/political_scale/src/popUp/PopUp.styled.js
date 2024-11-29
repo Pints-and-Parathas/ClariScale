8eb9c312d2651504709c718e8649ad03269417b0
james <jamespawson64@gmail.com>
2024-11-29 19:12:32 +0000

    Change z-index of plugin Remove default popup

--- frontend/political_scale/src/popUp/PopUp.styled.js
+++ frontend/political_scale/src/popUp/PopUp.styled.js
@@ -1,27 +1,31 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Styled component for the popup container
 export const PopupContainer = styled.div`
   position: fixed;
-  top: 0px; 
+  top: 0px;
   right: 0px;
   width: 250px;
   height: 50px;
   padding: 20px;
-  background-color: ${(props) => props.theme.background}; /* Use theme for background */
+  background-color: ${(props) =>
+    props.theme.background}; /* Use theme for background */
   color: ${(props) => props.theme.color}; /* Use theme for text color */
-  box-shadow: ${(props) => props.theme.shadow}; 
+  box-shadow: ${(props) => props.theme.shadow};
   border-radius: 10px;
-  z-index: 1000;
+  pointer-events: auto;
+  z-index: 999999;
 `;
 
 export const DismissButton = styled.button`
   position: absolute;
   top: 5px;
   right: 5px;
-  background-color: ${(props) => props.theme.dismissButtonBg || '#ccc'}; /* Default background color */
+  background-color: ${(props) =>
+    props.theme.dismissButtonBg || "#ccc"}; /* Default background color */
   border: none;
-  color: ${(props) => props.theme.dismissButtonColor || '#fff'}; /* White color for the cross */
+  color: ${(props) =>
+    props.theme.dismissButtonColor || "#fff"}; /* White color for the cross */
   font-size: 12px;
   width: 20px;
   height: 20px;
@@ -34,10 +38,11 @@ export const DismissButton = styled.button`
   z-index: 1011;
 
   &:hover {
-      background-color: ${(props) => props.theme.dismissButtonHoverBg || '#999'}; /* Hover background */
+    background-color: ${(props) =>
+      props.theme.dismissButtonHoverBg || "#999"}; /* Hover background */
   }
 
   &:focus {
-      outline: none;
+    outline: none;
   }
 `;
