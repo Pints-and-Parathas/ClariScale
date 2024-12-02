b81b833607ebfbe968a5198670220c0484e4d7be
Jaymeid <56159890+Jaymeid@users.noreply.github.com>
2024-12-02 00:39:20 +0000

    Update settings.py

--- backend/political_scale/political_scale/settings.py
+++ backend/political_scale/political_scale/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-vow1sft=ci*tal_$*!1_bfx_^%n%%(lvm$t*&_lg)e*@s1_u*n'
 
-ALLOWED_HOSTS = ['chrome-extension://mlbjhoppedlcbgnnpgmmjfgilnfdbdjc', '127.0.0.1']
+ALLOWED_HOSTS = ['chrome-extension://mlbjhoppedlcbgnnpgmmjfgilnfdbdjc', '127.0.0.1', '35.235.124.202']
 CSRF_TRUSTED_ORIGINS = ['chrome-extension://mlbjhoppedlcbgnnpgmmjfgilnfdbdjc']
 
 CORS_ORIGIN_ALLOW_ALL = True
